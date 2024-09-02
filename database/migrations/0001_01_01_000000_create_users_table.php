19978932e5d7f309fd07ee6c1e9dfad2aa41c852
Harshavardhan Singam <harshavardahn singam>
2024-09-02 20:41:29 +0530

    Apply tenant scope restriction to user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role')->default('admin');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('tenant_id')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
